25b5b30dc397fb5826f8cfbe55ca2601d0aa0f3b
tyl0r3 <tylerm874@gmail.com>
2016-09-27 18:36:09 -0400

    Added second line of BeerSong to loop

--- BeerSong/src/beersong/BeerSong.java
+++ BeerSong/src/beersong/BeerSong.java
@@ -8,6 +8,8 @@ public class BeerSong {
     public static void  Ninety_Nine_Bottles_of_Beer(){
        for(int i=99; i > 0; i--){
        System.out.println(i +" Bottles of beer on the wall, " + i +" bottles of beer");
+       System.out.println("Take one down, pass it around, " + (i-1) + " bottles of beer on the wall");
+
        }
      //this method will loop through and sing the song 
     }
