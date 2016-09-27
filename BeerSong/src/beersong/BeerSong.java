81efcfb1fa7d16d3af5d035e9e34c749c9c26fdf
tyl0r3 <tylerm874@gmail.com>
2016-09-27 18:33:02 -0400

    added basic structure of for loop

--- BeerSong/src/beersong/BeerSong.java
+++ BeerSong/src/beersong/BeerSong.java
@@ -6,7 +6,9 @@ package beersong;
 public class BeerSong {
   
     public static void  Ninety_Nine_Bottles_of_Beer(){
-   
+       for(int i=99; i > 0; i--){
+       
+       }
      //this method will loop through and sing the song 
     }
    
