9de2e44e3d3148a15ecd336bef0cc1e90e4d3a40
tyl0r3 <tylerm874@gmail.com>
2016-09-27 18:22:36 -0400

    Created BeerSong Netbeans Project with basic methods

---
+++ BeerSong/src/beersong/BeerSong.java
@@ -0,0 +1,19 @@
+
+package beersong;
+
+//author @tylerm874@gmail.com
+
+public class BeerSong {
+  
+    public static void  Ninety_Nine_Bottles_of_Beer(){
+   
+     //this method will loop through and sing the song 
+    }
+   
+    
+    public static void main(String[] args) {
+        //this calls the bottles of beer method 
+       Ninety_Nine_Bottles_of_Beer();
+    }
+    
+}
